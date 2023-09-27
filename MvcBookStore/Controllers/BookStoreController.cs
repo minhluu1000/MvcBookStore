3c0b8b1810f5a5ef8f70d989c003cb483f3b5d47
Lưu Quốc Minh <21dh113878@st.huflit.edu.vn>
2023-09-27 16:00:34 +0700

    Lab_B2

---
+++ MvcBookStore/Controllers/BookStoreController.cs
@@ -0,0 +1,36 @@
+﻿using MvcBookStore.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+namespace MvcBookStore.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        // Use DbContext to manage database
+        QLBANSACHEntities database = new QLBANSACHEntities();
+        private List<SACH> LaySachMoi(int soluong)
+        {
+            // Sắp xếp sách theo ngày cập nhật giảm dần, lấy đúng số lượng sách cần
+            // Chuyển qua dạng danh sách kết quả đạt được
+            return database.SACHes.OrderByDescending(sach =>
+           sach.Ngaycapnhat).Take(soluong).ToList();
+        }
+        // GET: BookStore
+        public ActionResult Index()
+        {
+            // Giả sử cần lấy 5 quyển sách mới cập nhật
+            var dsSachMoi = LaySachMoi(5);
+            return View(dsSachMoi);
+        }
+        public ActionResult LayChuDe()
+        {
+            var dsChuDe = database.CHUDEs.ToList();
+            return PartialView(dsChuDe);
+        }
+        public ActionResult LayNhaXuatBan()
+        {
+            var dsNhaXB = database.NHAXUATBANs.ToList();
+            return PartialView(dsNhaXB);
+        }
+    }
+}
